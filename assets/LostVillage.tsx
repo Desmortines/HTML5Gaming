724056282de0ec7b312fcd5b117724da4dddfc7f
Unknown <loic.dott@gmail.com>
2018-11-18 23:37:42 +0100

    backgroundchange, music added

---
+++ assets/LostVillage.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="LostVillage" tilewidth="32" tileheight="32" tilecount="13824" columns="128">
+ <image source="LostVillage.png" trans="5555ff" width="4096" height="3456"/>
+</tileset>
---
+++ assets/test.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="test" tilewidth="32" tileheight="32" tilecount="405" columns="27">
+ <image source="DownOfSorrow.png" trans="5555ff" width="864" height="480"/>
+</tileset>
